2d3e8f342382667500759d2fbd0f62a7f0803dbf
Grzegorz Nowak <strange3studio@gmail.com>
2019-08-01 11:20:23 +0200

    install python-dev headers

--- provisioning/bootstrap_provisioning.sh
+++ provisioning/bootstrap_provisioning.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-sudo apt install virtualenv build-essential nginx
+sudo apt install virtualenv build-essential nginx python-dev
 
 virtualenv provisioningenv
 . provisioningenv/bin/activate
